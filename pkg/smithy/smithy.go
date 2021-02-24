2da1f16c83f871d0330026474cf10004b301d39e
Doug Hellmann <doug@doughellmann.com>
2021-02-24 14:00:18 -0500

    templates: use consistent base template context
    Always construct the template context with site-wide settings and add
    any local data that is specific to the template being rendered.
    
    Signed-off-by: Doug Hellmann <doug@doughellmann.com>

--- pkg/smithy/smithy.go
+++ pkg/smithy/smithy.go
@@ -208,22 +208,37 @@ func RenderSyntaxHighlighting(file *object.File) (string, error) {
 }
 
 func Http404(ctx *gin.Context) {
-	ctx.HTML(http.StatusNotFound, "404.html", gin.H{})
+	smithyConfig := ctx.MustGet("config").(SmithyConfig)
+	ctx.HTML(http.StatusNotFound, "404.html", makeTemplateContext(smithyConfig, gin.H{}))
 }
 
 func Http500(ctx *gin.Context) {
-	ctx.HTML(http.StatusInternalServerError, "500.html", gin.H{})
+	smithyConfig := ctx.MustGet("config").(SmithyConfig)
+	ctx.HTML(http.StatusInternalServerError, "500.html",
+		makeTemplateContext(smithyConfig, gin.H{}))
+}
+
+func makeTemplateContext(config SmithyConfig, extra gin.H) gin.H {
+	results := gin.H{
+		"Site": gin.H{
+			"Title":       config.Title,
+			"Description": config.Description,
+			"Host":        config.Host,
+		},
+	}
+	for k, v := range extra {
+		results[k] = v
+	}
+	return results
 }
 
 func IndexView(ctx *gin.Context, urlParts []string) {
 	smithyConfig := ctx.MustGet("config").(SmithyConfig)
 	repos := smithyConfig.GetRepositories()
 
-	ctx.HTML(http.StatusOK, "index.html", gin.H{
-		"Repos":       repos,
-		"Title":       smithyConfig.Title,
-		"Description": smithyConfig.Description,
-	})
+	ctx.HTML(http.StatusOK, "index.html", makeTemplateContext(smithyConfig, gin.H{
+		"Repos": repos,
+	}))
 }
 
 func findMainBranch(ctx *gin.Context, repo *git.Repository) (string, *plumbing.Hash, error) {
@@ -286,15 +301,13 @@ func RepoIndexView(ctx *gin.Context, urlParts []string) {
 		}
 	}
 
-	ctx.HTML(http.StatusOK, "repo-index.html", gin.H{
+	ctx.HTML(http.StatusOK, "repo-index.html", makeTemplateContext(smithyConfig, gin.H{
 		"Name":     repoName,
 		"Branches": bs,
 		"Tags":     ts,
 		"Readme":   template.HTML(formattedReadme),
 		"Repo":     repo,
-
-		"Host": smithyConfig.Host,
-	})
+	}))
 }
 
 func RepoGitView(ctx *gin.Context, urlParts []string) {
@@ -338,11 +351,11 @@ func RefsView(ctx *gin.Context, urlParts []string) {
 		ts = []*plumbing.Reference{}
 	}
 
-	ctx.HTML(http.StatusOK, "refs.html", gin.H{
+	ctx.HTML(http.StatusOK, "refs.html", makeTemplateContext(smithyConfig, gin.H{
 		"Name":     repoName,
 		"Branches": bs,
 		"Tags":     ts,
-	})
+	}))
 }
 
 func TreeView(ctx *gin.Context, urlParts []string) {
@@ -414,12 +427,12 @@ func TreeView(ctx *gin.Context, urlParts []string) {
 	if treePath == "" {
 		entries := ConvertTreeEntries(tree.Entries)
 
-		ctx.HTML(http.StatusOK, "tree.html", gin.H{
+		ctx.HTML(http.StatusOK, "tree.html", makeTemplateContext(smithyConfig, gin.H{
 			"RepoName": repoName,
 			"RefName":  refNameString,
 			"Files":    entries,
 			"Path":     treePath,
-		})
+		}))
 		return
 	}
 
@@ -438,14 +451,14 @@ func TreeView(ctx *gin.Context, urlParts []string) {
 			return
 		}
 		entries := ConvertTreeEntries(subTree.Entries)
-		ctx.HTML(http.StatusOK, "tree.html", gin.H{
+		ctx.HTML(http.StatusOK, "tree.html", makeTemplateContext(smithyConfig, gin.H{
 			"RepoName":   repoName,
 			"ParentPath": parentPath,
 			"RefName":    refNameString,
 			"SubTree":    out.Name,
 			"Path":       treePath,
 			"Files":      entries,
-		})
+		}))
 		return
 	}
 
@@ -464,7 +477,7 @@ func TreeView(ctx *gin.Context, urlParts []string) {
 		Http404(ctx)
 		return
 	}
-	ctx.HTML(http.StatusOK, "blob.html", gin.H{
+	ctx.HTML(http.StatusOK, "blob.html", makeTemplateContext(smithyConfig, gin.H{
 		"RepoName":            repoName,
 		"RefName":             refNameString,
 		"File":                out,
@@ -472,7 +485,7 @@ func TreeView(ctx *gin.Context, urlParts []string) {
 		"Path":                treePath,
 		"Contents":            contents,
 		"ContentsHighlighted": template.HTML(syntaxHighlighted),
-	})
+	}))
 }
 
 func LogView(ctx *gin.Context, urlParts []string) {
@@ -532,11 +545,11 @@ func LogView(ctx *gin.Context, urlParts []string) {
 		commits = append(commits, c)
 	}
 
-	ctx.HTML(http.StatusOK, "log.html", gin.H{
+	ctx.HTML(http.StatusOK, "log.html", makeTemplateContext(smithyConfig, gin.H{
 		"Name":    repoName,
 		"RefName": refNameString,
 		"Commits": commits,
-	})
+	}))
 }
 
 func LogViewDefault(ctx *gin.Context, urlParts []string) {
@@ -642,11 +655,11 @@ func CommitView(ctx *gin.Context, urlParts []string) {
 		return
 	}
 
-	ctx.HTML(http.StatusOK, "commit.html", gin.H{
+	ctx.HTML(http.StatusOK, "commit.html", makeTemplateContext(smithyConfig, gin.H{
 		"Name":    repoName,
 		"Commit":  commitObj,
 		"Changes": template.HTML(formattedChanges),
-	})
+	}))
 }
 
 func ListBranches(r *git.Repository) ([]*plumbing.Reference, error) {
